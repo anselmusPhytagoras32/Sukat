3221c80dbbde88fb06ed7af5e31067af147741d3
anselmusPhytagoras32 <gebhel.anselm@gmail.com>
2026-03-20 14:28:04 +0800

    feat: security for password hashing

---
+++ backend/src/core/security.py
@@ -0,0 +1,15 @@
+import bcrypt
+
+def hash_password(password: str) -> str:
+    pwd_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(pwd_bytes, salt)
+    
+    return hashed_password.decode('utf-8')
+
+def verify_hash(password: str, hashed_pass: str) -> bool:
+    # Convert both the login attempt and the database hash into bytes for comparison 
+    password_byte_enc = password.encode('utf-8')
+    hashed_pass_byte_enc = hashed_pass.encode('utf-8')
+    
+    return bcrypt.checkpw(password_byte_enc, hashed_pass_byte_enc)
